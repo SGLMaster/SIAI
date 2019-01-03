78c4ec5fd6452ec71b04a40f1593a3f96c96508d
Willians Briceño <williambriceo567@gmail.com>
2019-01-03 15:16:35 -0400

    Added python script to fix the header issue with the generated mapeditor.cpp wxFormBuilder file.

--- scripts/move-forms.bat
+++ scripts/move-forms.bat
@@ -3,3 +3,9 @@ cd ../
 if exist "mapeditor.cpp" (move "mapeditor.cpp" "src/siai/editor/forms")
 
 if exist "mapeditor.h" (move "mapeditor.h" "include/siai/editor/forms")
+
+python fix-mapeditor-include.py
+
+echo "Done"
+
+pause
