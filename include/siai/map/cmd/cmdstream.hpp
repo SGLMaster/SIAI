6cfdbad3a7e2b750769c5c7c69b64548b7566b40
Willians Briceño <williambriceo567@gmail.com>
2019-01-09 17:46:22 -0400

    Reverted adding method for saving the changes of the last commad.

--- include/siai/map/cmd/cmdstream.hpp
+++ include/siai/map/cmd/cmdstream.hpp
@@ -14,8 +14,6 @@ public:
     static std::unique_ptr<CommandStream> create();
 
     virtual void executeAndLog(Entities::Container& entities, const std::string& command) = 0;
-    virtual void saveLastCommandChangeToDb(DbConnector& connector, const std::string& mapName) = 0;
-
     virtual void undo(Entities::Container& entities) = 0;
     virtual void redo(Entities::Container& entities) = 0;
 };
