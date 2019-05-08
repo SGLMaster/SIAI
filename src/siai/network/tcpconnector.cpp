c7d45d60c9c2bf507111bea5326e89e0bebbb400
Willians Briceño <williambriceo567@gmail.com>
2019-05-08 11:38:01 -0400

    First implementation of "TcpConnector::open()".

--- src/siai/network/tcpconnector.cpp
+++ src/siai/network/tcpconnector.cpp
@@ -1,5 +1,7 @@
 #include "network/tcpconnector.hpp"
 
+#include "log.hpp"
+
 TcpConnector::TcpConnector(int serverPort, int socketPort, IMapEntity* parent) : wxEvtHandler(), 
                                                                                 m_serverPort{serverPort},
                                                                                 m_socketPort{socketPort}, 
@@ -15,7 +17,40 @@ TcpConnector::~TcpConnector()
 
 void TcpConnector::open()
 {
+    //Create and set address to 0.0.0.0:m_serverPort
+    wxIPV4address address;
+    address.Service(m_serverPort);
 
+    Log::simpleMessage( std::string("Creando servidor en ") + address.IPAddress().ToStdString() + ":" 
+                        + std::to_string(address.Service()) );
+
+    m_server = std::make_unique<wxSocketServer>(address);
+
+    if(!m_server->IsOk())
+    {
+        Log::warning("No se puede usar el puerto especificado!");
+        return;
+    }
+
+    wxIPV4address realAddress;
+    if ( !m_server->GetLocal(realAddress) )
+    {
+        Log::warning("Error: No se pudo obtener la direccion!");
+        return;
+    }
+    else
+    {
+        Log::warning( std::string("Servidor escuchando en %s:%u") + realAddress.IPAddress().ToStdString() 
+                    + std::to_string(realAddress.Service()) );
+    }
+
+    //Setup the event handler and subscribe to connection events
+    m_server->SetEventHandler(*this, m_serverPort);
+    m_server->SetNotify(wxSOCKET_CONNECTION_FLAG);
+    m_server->Notify(true);
+
+    //Connect the events to a handler function
+    Bind(wxEVT_SOCKET, &TcpConnector::OnServerEvent, this, m_serverPort);
 }
 
 void TcpConnector::close()
