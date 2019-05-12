03ff86925d5a193a0a8dcb2cc92c8378f5e4be5d
Willians Briceño <williambriceo567@gmail.com>
2019-05-12 10:23:53 -0400

    Fixed "Log::timestamp" for GUI apps.

--- src/siai/log.cpp
+++ src/siai/log.cpp
@@ -21,7 +21,7 @@ void Log::timestamp(const std::string& msg, bool logToCmd)
     if(logToCmd)
         wxPrintf( wxT("[%.2u:%.2u:%.2u] %s\n"), curHour, curMinute, curSecond, _(msg) );
     else
-        wxMessageBox(_(msg), "Log");
+        wxMessageBox(wxString::Format("[%.2u:%.2u:%.2u] %s", curHour, curMinute, curSecond, _(msg)), "Log");
 }
 
 void Log::warning(const std::string& msg, bool logToCmd)
